42870fce8821822ab61d313cbbd27d41ec468d04
Vasileios <vasileios@contractuo.com>
2024-03-04 01:32:17 +0200

    Added basic table/fetched data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
+  <div class="p-8 bg-gray-200 min-h-screen">
+    <DataTable :data-sets="dataSets" /> 
+  </div>
 </template>
 
-<script setup>
-</script>
+<script>
+// import axios from "axios";
+import DataTable from "./components/DataTable.vue";
+import timeseries from "/timeseries.json"
 
-<style scoped>
-</style>
+export default {
+  components: {
+    DataTable
+  },
+
+  data() {
+    return {
+      dataSets: timeseries 
+    };
+  },
+
+  // mounted() {
+  //   axios.get("/timeseries.json").then(response => {
+  //     console.log(response)
+  //     this.dataSets = response.data;
+  //     console.log(this.dataSets)
+  //   }).catch(error => {
+  //     console.error("Error fetching data:", error);
+  //   });
+  // }
+};
+</script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-white relative border rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-100">
+        <tr>
+          <th class="px-4 py-3">Date & Time</th>
+          <th class="px-4 py-3">DE_Price (€)</th>
+          <th class="px-4 py-3">GR_Price (€)</th>
+          <th class="px-4 py-3">FR_Price (€)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in dataSets" :key="index">
+          <td class="px-4 py-3 font-medium text-gray-900"> {{ data.DateTime }} </td>
+          <td class="px-4 py-3 font-medium text-gray-900"> {{ data.ENTSOE_DE_DAM_Price }} </td>
+          <td class="px-4 py-3 font-medium text-gray-900"> {{ data.ENTSOE_GR_DAM_Price }} </td>
+          <td class="px-4 py-3 font-medium text-gray-900"> {{ data.ENTSOE_FR_DAM_Price }} </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataSets:{
+      type: Array,
+      required: true
+    }
+  },
+}
+</script>
